3cf40c311fe3a0830b6d57ee032d2e3e2869012d
Cole Perry <colecperry@gmail.com>
2023-04-05 15:51:27 -0700

    Done

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import ipdb
 
 APPROVED_JOBS = [
     "Admin",
@@ -15,5 +16,40 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+# class Person:
+#     def __init__(self, name, job):
+#         if type(name) == str and (1 <= len(name) <= 25):
+#             self.name = name.title()
+#         else:
+#             print("Name must be string between 1 and 25 characters.")
+
 class Person:
-    pass
+    def __init__(self, name="Jim", job="Admin"):
+        self.name = name
+        self.job = job
+
+    def get_name (self):
+        return self._name
+    
+    def set_name (self,name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            self._name = name.title()
+        else: 
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name,set_name)
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self,job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+
+
+    
+
